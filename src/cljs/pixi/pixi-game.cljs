38a7dbf9ae3f89cfbabc65b695cb287478c9d3f5
Devon Tackett <frogplague@gmail.com>
2014-07-08 21:04:34 -0700

    Crude implementation of cloning a game.

--- src/cljs/pixi/pixi-game.cljs
+++ src/cljs/pixi/pixi-game.cljs
@@ -80,24 +80,58 @@
                                                         :rotation 0
                                                         :anchor {:x 0.5 :y 0.5}
                                                         :update-fn
-                                                        (fn
-                                                          [e]
-                                                          (assoc
-                                                            e
-                                                            :rotation
-                                                            (-
-                                                             (:rotation e)
-                                                             0.1)))})}))
+                                                          (fn
+                                                            [e]
+                                                            (assoc
+                                                              e
+                                                              :rotation
+                                                              (-
+                                                               (:rotation e)
+                                                               0.1)))})}))
+
+(def simple-world-b (build-pixi-world {1 (build-pixi-sprite {
+                                                        :id 1
+                                                        :texture my-bunny
+                                                        :position {:x 50 :y 50}
+                                                        :rotation 0
+                                                        :anchor {:x 0.5 :y 0.5}
+                                                        :update-fn
+                                                          (fn
+                                                            [e]
+                                                            (assoc
+                                                              e
+                                                              :position
+                                                              { :x 50
+                                                               :y (+ 50 (* 30 (.sin js/Math (/ (.now js/Date) 200))))}))})}))
 
 (simple-add-game test-games (build-pixi-game
                   :my-game
-                  (build-pixi-view (.-body js/document) 400 300 0xaa6699)
+                  (build-pixi-view (.-body js/document) 100 100 0xaa6699)
                   simple-world))
 
 (simple-add-game test-games
- (build-pixi-game :my-game-2
-                  (build-pixi-view (.-body js/document) 400 400 0xaaaa99)
-                  simple-world))
+ (build-pixi-game :my-other-game
+                  (build-pixi-view (.-body js/document) 100 100 0xaa66ee)
+                  simple-world-b))
+
+(defn clone-game!
+  [clone-id new-id]
+  (simple-add-game
+   test-games
+   (build-pixi-game
+    new-id
+    (build-pixi-view
+     (.-body js/document)
+     100 100
+     0x55aaee)
+    (:world (find-game clone-id @test-games)))))
+
+(defn find-game
+  [id games]
+  (cond (= id (:id (first games))) (first games)
+        :else (find-game id (rest games))))
+
+(clone-game! :my-game :my-game-clone)
 
 ;; setup animation loop
 (defn animate[]
